a61710db96d043eff33d17feec49bd74eee85daf
Philo Kaulkin <48894630+Phil0nator@users.noreply.github.com>
2021-04-26 18:06:14 -0700

    async tasks

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,13 +1,29 @@
 #include "../src/async.hpp"
 #include <future>
+
+int test (){
+    std::this_thread::sleep_for(std::chrono::seconds(1)); 
+    return 5; 
+}
+
+
 int main(){
     
 
-    auto dispose = async::mainloop.execute<int>( [](){ std::this_thread::sleep_for(std::chrono::seconds(1)); return 5; } );
-    async::mainloop.execute( [](){ std::this_thread::sleep_for(std::chrono::seconds(100)); } );
-    puts("Here");
+    async::EventLoop loop(4);
 
-    std::cout << dispose.await() << std::endl;
+    // auto dispose = loop.execute<int>( test );
+    // auto dispose2 = loop.execute( [](){ 
+    //     std::this_thread::sleep_for(std::chrono::seconds(10)); 
+    //     } );
+
+    // std::cout << dispose->await() << std::endl;
+    // dispose2->await();
+
+    std::array test{1,2,3,4,5,6,7,8,10};
+    auto disposable = async::algorithm::to_each( loop, test.begin(), test.end(), [](auto& i){ std::cout << i << std::endl; } );
+
+    disposable->await();
 
     return 0;
 }
--- src/async.hpp
+++ src/async.hpp
@@ -13,27 +13,30 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
-
-#if __cplusplus < 200000L
-#error "async.hpp requires c++20 or later"
-#endif
+#include <iterator>
+#include <concepts>
 
 namespace async{
 
-
-
     template<typename T>
-    class Promise{
+    class _Promise{
 
         public:
         typedef std::optional<T> maybeT;
         typedef std::function< maybeT (T&)> Thentype;
         typedef T type; 
-        Promise() = default;
-        Promise(const Promise<T>& other) : element(other.element), thenfn(other.thenfn) { set = (bool)other.set; };
-        ~Promise() = default;
+        _Promise() = default; 
+        _Promise(const _Promise<T>& other) : element(other.element), thenfn(other.thenfn) { set = (bool)other.set; };
+        ~_Promise() = default;
+        /**
+         * await() will block the thread until this promise is fulfilled.
+         * @returns the value assigned to this promise;
+         */
         T& await()
             { while(!set){} return **element; }
+        /**
+         * 
+         */
         maybeT block_then( Thentype fn ) 
             {  return fn(await()); }
         
@@ -44,26 +47,32 @@ namespace async{
             if(set) return **element ;
             else return std::nullopt;
         }
-        Promise<T> then(const Thentype& fn) 
-        {
-            Promise<T> out;
-            thenfn = [&](T& v){ out.assign(fn(v)); return std::nullopt;};
-            return out;
-        }
-        void assign(const maybeT& value)
+        
+        void assign(maybeT value)
         { 
-            element =  std::make_shared<maybeT>(value); 
+            if(element.get() == nullptr){
+                element = std::make_shared<maybeT>(value);
+            }else{
+                *element =  (value); 
+            }
             set = true; 
-            if(thenfn and *element) thenfn.operator*()(**element);
         }
+
+        bool done(){
+            return set;
+        }
+
+        
 
 
         private:
-        std::shared_ptr<maybeT> element;
+        std::shared_ptr<maybeT> element ;
         std::atomic<bool> set = false;
         std::optional<Thentype> thenfn = std::nullopt;
     };
 
+    template<typename T>
+    using Promise = std::unique_ptr<_Promise<T > >; 
 
     template<typename T> 
     class Queue{
@@ -75,26 +84,26 @@ namespace async{
 
         void enqueue(const T& x) 
         { 
-            std::lock_guard lock(mut);
+            std::lock_guard<std::mutex> lock(mut);
             base.emplace(x); 
         }
 
         bool empty()
         {
-            std::lock_guard lock(mut);
+            std::lock_guard<std::mutex> lock(mut);
             bool out = base.empty();
             return out;
         }
 
         auto dequeue(){
-            std::lock_guard lock(mut);
+            std::lock_guard<std::mutex> lock(mut);
             auto out = base.front();
             base.pop();
             return out;
         }
 
         std::optional<T> safeget(){
-            std::lock_guard lock(mut);
+            std::lock_guard<std::mutex> lock(mut);
             if(!base.empty()){
                 auto out =  base.front();
                 base.pop();
@@ -109,58 +118,114 @@ namespace async{
         mutable std::mutex mut;
     };
 
-    template<size_t worker_count = 4>
+    
     class EventLoop{
 
         public:
-
-        EventLoop()
-        {
+        std::atomic<size_t> activeThreads;
+        std::atomic<bool> isActive = true;
+        EventLoop(size_t workers = 4)
+        {   
+            worker_count = workers;
             for(size_t i = 0; i < worker_count; i++){
-                std::thread t( worker_mainloop, task_handle );
-                t.detach();
+                threadpool.push_back( std::thread( worker_mainloop, task_handle, this ) );
             }
         }
 
         EventLoop(const EventLoop& other) = delete;
-        ~EventLoop() = default;
-
+        ~EventLoop(){
+            isActive = false;
+            for(auto& t: threadpool){
+                t.join();
+            }
+        
+        }
         template<typename T>
         [[nodiscard]] Promise<T> execute( std::function<T ()> coroutine) 
             {
-                Promise<T> out;
+                _Promise<T> out;
+                Promise<T> ptr = std::make_unique<_Promise<T> >(out);
+                if (activeThreads == threadpool.size()) // if all threads are occupied
+                {
+                    threadpool.push_back(std::thread( worker_mainloop, task_handle, this ));
+                    worker_count++;
+                }
                 task_handle->enqueue( 
-                    [&](){ out.assign(coroutine()); }
+                    [coroutine, &ptr](){ 
+                        ptr->assign(coroutine());
+                    }
                 );
-                return out;
+                return ptr;
             }
-        void execute( std::function<void(void)> coroutine )
-            { task_handle->enqueue(coroutine); }
+        [[nodiscard]] Promise<bool> execute( std::function<void(void)> coroutine )
+            { return execute<bool>( [=](){ coroutine(); return true;} ); }
+
+        
+        
+
 
         private:
         typedef std::shared_ptr< Queue< std::function<void (void)> > > task_handle_t;
-        
         typedef task_handle_t::weak_type weak_task_t;
         typedef Queue<task_handle_t::element_type::element_t> queue_t;
-        
+        size_t worker_count = 4;
+        std::vector<std::thread> threadpool;
+
         task_handle_t task_handle = std::make_shared< queue_t >( queue_t() );
-        static void worker_mainloop(weak_task_t activeSignal){
+
+
+        static void worker_mainloop(weak_task_t activeSignal, EventLoop* rawParent){
             while(!activeSignal.expired()){
+                if (rawParent->isActive == false){
+                    exit(0); // exit thread;
+                }
                 std::optional<std::function<void ()>> maybeTask;
                 {
                     auto tasks = activeSignal.lock();
                     maybeTask = tasks->safeget();
                 }
                 if (maybeTask) {
+                    rawParent->activeThreads++;
                     (*maybeTask)();
+                    rawParent->activeThreads--;
                 }
                 std::this_thread::sleep_for( std::chrono::milliseconds(1) );
             }
         }
 
+       
+
     };
 
 
-    EventLoop<4> mainloop;
+    namespace algorithm{
+        
+        template<class InputIterator, typename Function> 
+        //TODO Fix std::invokable for lambda/funs
+            requires std::input_iterator<InputIterator> && std::invocable<Function>
+        [[nodiscard]] Promise<size_t> to_each(EventLoop& e, InputIterator begin, InputIterator end, Function fn){
+            
+            std::for_each(begin, end-1, [&e,fn, begin, end](auto& i){
+                e.execute(
+                    [&](){
+                        std::invoke(fn, i);
+                    }
+                );
+            
+            
+            });
+
+            return e.execute( [=](){ std::invoke(fn, *end); } );
+
+        }
+
+
+    }
+
+
+
+
+
+    //EventLoop<4> mainloop;
 
 };
